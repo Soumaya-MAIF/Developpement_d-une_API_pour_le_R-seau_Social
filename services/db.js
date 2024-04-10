2c46f70dd1ae5db4b0aab141da9e365280c733e5
Soum <dev.web21@outlook.fr>
2024-04-10 10:35:50 +0200

    creation fichier db + dossiers

---
+++ services/db.js
@@ -0,0 +1,12 @@
+// services/db.js
+import { Pool } from 'pg'
+
+const pool = new Pool({
+  user: process.env.PG_USER ?? 'postgres',
+  host: process.env.PG_HOST ?? 'localhost',
+  database: process.env.PG_DATABASE ?? 'my_social_network',
+  password: process.env.PG_PASSWORD ?? 'postgres',
+  port: process.env.PG_PORT ?? 5432
+})
+
+export default pool
